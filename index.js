4340e769f225c21da6afc96d6d10fb125df67913
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2019-04-17 16:51:17 +0200

    new test picam

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const { StreamCamera, Codec } = require('pi-camera-connect');
-const fs = require('fs');
+const picam = require('node-picamera');
 
-const runApp = async () => {
-	const streamCamera = new StreamCamera({
-		codec: Codec.H264
-	});
-
-	const videoStream = streamCamera.createStream();
-
-	const writeStream = fs.createWriteStream('video-stream.h264');
-
-	// Pipe the video stream to our video file
-	videoStream.pipe(writeStream);
-
-	await streamCamera.startCapture();
-
-	// We can also listen to data events as they arrive
-	videoStream.on('data', data => console.log('New data', data));
-	videoStream.on('end', data => console.log('Video stream has ended'));
-
-	// Wait for 5 seconds
-	await new Promise(resolve => setTimeout(() => resolve(), 5000));
-
-	await streamCamera.stopCapture();
-};
-
-runApp();
+picam.video('filename.h264', 1000, {}, function(err, stdin, stdout) {
+	if (err) throw err;
+});
