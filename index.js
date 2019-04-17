9389e8e8c2f8d519dd9bd5c06abf28ff40467629
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2019-04-17 18:13:58 +0200

    ...

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-// require module
-const raspberryPiCamera = require('raspberry-pi-camera-native');
+const { StreamCamera, Codec } = require('pi-camera-connect');
+const fs = require('fs');
 
-// add frame data event listener
-raspberryPiCamera.on('frame', frameData => {
-	// frameData is a Node.js Buffer
-    // ...
-    console.log(frameData);
-});
+const runApp = async () => {
+	const streamCamera = new StreamCamera({
+		codec: Codec.H264
+	});
 
-// start capture
-raspberryPiCamera.start();
+	const videoStream = streamCamera.createStream();
+
+	const writeStream = fs.createWriteStream('video-stream.mp4');
+
+	// Pipe the video stream to our video file
+	videoStream.pipe(writeStream);
+
+	await streamCamera.startCapture();
+
+	// We can also listen to data events as they arrive
+	videoStream.on('data', data => console.log('New data', data));
+	videoStream.on('end', data => console.log('Video stream has ended'));
+
+	// Wait for 5 seconds
+	await new Promise(resolve => setTimeout(() => resolve(), 5000));
+
+	await streamCamera.stopCapture();
+};
+
+runApp();
