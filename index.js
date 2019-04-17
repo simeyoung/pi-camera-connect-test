b822a4ca8eec3f60aa8dcf9591fa87cd1c453069
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2019-04-17 16:52:43 +0200

    correct

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const picam = require('node-picamera');
 
 picam.video('filename.h264', 1000, {}, function(err, stdin, stdout) {
-	if (err) throw err;
+    if (err) throw err;
+    console.log('stdin: ', stdin);
+    console.log('stdout: ', stdout);
 });
